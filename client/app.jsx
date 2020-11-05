a422d6c43ee72399a34d0fa40cd301881ae26870
Rasha <r.alamleh@gmail.com>
2020-11-05 15:17:33 +0200

    What IS THIS

--- client/app.jsx
+++ client/app.jsx
@@ -3,10 +3,32 @@ import css from './styles.css';
 
 class app extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-       
-      };
+        super(props);
+        this.state = {
+
+        };
+    }
+    render() {
+        return (
+            <div>
+                <div class="all"></div>
+                <div id="Header" class="container">Tec Tac Toe
+                        <div class="panel" id="panel">
+                        <button id="1" onclick="move(this)"></button>
+                        <button id="2" onclick="move(this)"></button>
+                        <button id="3" onclick="move(this)"></button>
+
+                        <button id="4" onclick="move(this)"></button>
+                        <button id="5" onclick="move(this)"></button>
+                        <button id="6" onclick="move(this)"></button>
+
+                        <button id="7" onclick="move(this)"></button>
+                        <button id="8" onclick="move(this)"></button>
+                        <button id="9" onclick="move(this)"></button>
+                    </div>
+                </div>
+            </div>
+        )
     }
 }
 
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
+const express = require('express');
 
+const app = express();
+
+//app.use(express.static('public')
+
+//ROUTS 
+app.get("/" , (request , response) => {
+    response.send('We are at home');
+});
+
+//listen to the server.
+app.listen(3000);
 
