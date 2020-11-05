2caaf1268df177157b661fab6aceecaa35bfbbbf
Rasha <r.alamleh@gmail.com>
2020-11-05 11:19:38 +0200

    By works, I meant 'doesnt work'. Works now..

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import css from './styles.css';
+
+class app extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+       
+      };
+    }
+}
 
 
+ReactDOM.render(<app />, document.getElementById('app'));
